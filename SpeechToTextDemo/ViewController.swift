f3958e6a76a7e282af25f8add0c525dbe343a471
lincircle <cosine@yuhsuans-macbook-pro.local>
2016-09-29 10:20:58 +0800

    Initial Commit

---
+++ SpeechToTextDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpeechToTextDemo
+//
+//  Created by Yuhsuan Lin on 2016/9/29.
+//  Copyright © 2016年 me.cgua.speechtotext. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
